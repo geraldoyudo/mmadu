52f5c3dd13332580d003078915cce0982ce8d44c
Oyudo Gerald <gerald.oyudo@gmail.com>
2018-09-03 12:30:16 +0100

    set up project

---
+++ mmadu-user-service/src/main/java/com/mmadu/service/MmaduUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mmadu.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaduUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmaduUserServiceApplication.class, args);
+	}
+}
