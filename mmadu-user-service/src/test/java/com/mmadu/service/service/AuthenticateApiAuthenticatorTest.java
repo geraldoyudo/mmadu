2932a5b89c64074b75f780061ca9fe93e7577f85
Oyudo Gerald <gerald.oyudo@gmail.com>
2018-09-28 07:43:16 +0100

    updated tests

--- mmadu-user-service/src/test/java/com/mmadu/service/service/AuthenticateApiAuthenticatorTest.java
+++ mmadu-user-service/src/test/java/com/mmadu/service/service/AuthenticateApiAuthenticatorTest.java
@@ -15,10 +15,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = AuthenticateApiAuthenticator.class)
+@TestPropertySource(properties = "mmadu.domain.authenticate-api-security-enabled=true")
 public class AuthenticateApiAuthenticatorTest {
     public static final String VALID_DOMAIN = "1";
     public static final String VALID_TOKEN = "1234";
