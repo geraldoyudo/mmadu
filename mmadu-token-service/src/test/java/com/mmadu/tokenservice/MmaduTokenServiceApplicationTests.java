029f29b0f17413217b77ce6b5fe5db63aee30f59
Oyudo Gerald <gerald.oyudo@gmail.com>
2019-06-22 12:33:25 +0100

    upgraded spring version

---
+++ mmadu-token-service/src/test/java/com/mmadu/tokenservice/MmaduTokenServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mmadu.tokenservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MmaduTokenServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
