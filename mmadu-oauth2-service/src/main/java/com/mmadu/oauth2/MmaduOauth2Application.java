e58d4dc4ca83fcab285d8ae2293b99e58f32745c
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-05-27 06:33:50 +0100

    added mmadu oauth2 module

---
+++ mmadu-oauth2-service/src/main/java/com/mmadu/oauth2/MmaduOauth2Application.java
@@ -0,0 +1,13 @@
+package com.mmadu.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaduOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmaduOauth2Application.class, args);
+    }
+
+}
---
+++ mmadu-oauth2-service/src/test/java/com/mmadu/oauth2/MmaduOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmadu.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmaduOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
