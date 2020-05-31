068006ddbd5cff49359bf099949424d4aa931843
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-05-31 07:34:23 +0100

    implemented authenticate api

--- mmadu-identity/src/test/java/com/mmadu/identity/providers/users/MmaduUserServiceImplTest.java
+++ mmadu-identity/src/test/java/com/mmadu/identity/providers/users/MmaduUserServiceImplTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.junit.jupiter.MockServerExtension;
 import org.mockserver.junit.jupiter.MockServerSettings;
+import org.mockserver.matchers.MatchType;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.MediaType;
 import org.mockserver.verify.VerificationTimes;
@@ -16,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.TestPropertySource;
 
 import java.io.IOException;
@@ -26,6 +29,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.model.JsonBody.json;
 
 @ExtendWith(MockServerExtension.class)
 @SpringBootTest(classes = {
@@ -42,6 +46,14 @@ class MmaduUserServiceImplTest {
     private MmaduUserService userService;
     @Value("classpath:responses/get-user-by-username-and-domain.json")
     private Resource userFile;
+    @Value("classpath:requests/user-authentication-request.json")
+    private Resource userAuthenticateRequest;
+    @Value("classpath:responses/user-authentication-response.json")
+    private Resource userAuthenticateResponse;
+    @Value("classpath:responses/user-authentication-username-invalid.json")
+    private Resource invalidUsernameResponse;
+    @Value("classpath:responses/user-authentication-password-invalid.json")
+    private Resource invalidPasswordResponse;
 
     @AfterEach
     void reset(MockServerClient server) {
@@ -108,5 +120,53 @@ class MmaduUserServiceImplTest {
                                 .withStatusCode(404)
                 );
         return request;
+    }
+
+    @Test
+    void authenticate(MockServerClient server) throws Exception {
+        HttpRequest request = mockAuthenticateApi(server, userAuthenticateResponse);
+        userService.authenticate("1", "user", "password");
+        server.verify(request, VerificationTimes.once());
+    }
+
+    private HttpRequest mockAuthenticateApi(MockServerClient server, Resource response) throws Exception {
+        HttpRequest request = request()
+                .withMethod("POST")
+                .withPath("/domains/1/authenticate")
+                .withBody(
+                        json(
+                                IOUtils.toString(userAuthenticateRequest.getInputStream(), StandardCharsets.UTF_8),
+                                MatchType.STRICT
+                        )
+                )
+                .withHeader("domain-auth-token", "2222");
+        server.when(
+                request
+        )
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                                .withBody(
+                                        IOUtils.toString(response.getInputStream(), StandardCharsets.UTF_8),
+                                        MediaType.JSON_UTF_8
+                                )
+                );
+        return request;
+    }
+
+    @Test
+    void givenInvalidPasswordThenThrowBadCredentialsException(MockServerClient server) throws Exception {
+        HttpRequest request = mockAuthenticateApi(server, invalidPasswordResponse);
+        assertThrows(BadCredentialsException.class,
+                () -> userService.authenticate("1", "user", "password"));
+        server.verify(request, VerificationTimes.once());
+    }
+
+    @Test
+    void givenInvalidUsernameThenThrowBadCredentialsException(MockServerClient server) throws Exception {
+        HttpRequest request = mockAuthenticateApi(server, invalidUsernameResponse);
+        assertThrows(UsernameNotFoundException.class,
+                () -> userService.authenticate("1", "user", "password"));
+        server.verify(request, VerificationTimes.once());
     }
 }
