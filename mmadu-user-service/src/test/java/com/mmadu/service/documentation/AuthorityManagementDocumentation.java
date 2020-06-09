26f638d3c4fa70371e7043afa38fb2531b9dda65
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-06-09 09:46:05 +0100

    added authority documentation

--- mmadu-user-service/src/test/java/com/mmadu/service/documentation/AuthorityManagementDocumentation.java
+++ mmadu-user-service/src/test/java/com/mmadu/service/documentation/AuthorityManagementDocumentation.java
@@ -1,11 +1,13 @@
 package com.mmadu.service.documentation;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mmadu.service.entities.AppUser;
-import com.mmadu.service.entities.Group;
-import com.mmadu.service.entities.UserGroup;
-import com.mmadu.service.models.NewGroupRequest;
-import com.mmadu.service.repositories.GroupRepository;
-import com.mmadu.service.repositories.UserGroupRepository;
+import com.mmadu.service.entities.Authority;
+import com.mmadu.service.entities.Authority;
+import com.mmadu.service.entities.UserAuthority;
+import com.mmadu.service.repositories.AuthorityRepository;
+import com.mmadu.service.repositories.UserAuthorityRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 
-import java.util.List;
-
 import static com.mmadu.service.utilities.DomainAuthenticationConstants.DOMAIN_AUTH_TOKEN_FIELD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -26,122 +27,58 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class AuthorityManagementDocumentation extends AbstractDocumentation {
     @Autowired
-    private UserGroupRepository userGroupRepository;
+    private UserAuthorityRepository userAuthorityRepository;
     @Autowired
-    private GroupRepository groupRepository;
-
-    private Group group;
+    private AuthorityRepository authorityRepository;
+    private Authority authority;
 
     @BeforeEach
     void setUp() {
         appUserRepository.deleteAll();
-        userGroupRepository.deleteAll();
-        groupRepository.deleteAll();
+        userAuthorityRepository.deleteAll();
+        authorityRepository.deleteAll();
     }
 
     @Test
-    void createGroup() throws Exception {
-        createAndSaveGroup();
-        NewGroupRequest request = new NewGroupRequest();
-        request.setName("Test Group");
-        request.setIdentifier("test");
-        request.setDescription("A test Group");
-        request.setParentGroup(group.getIdentifier());
-
-        mockMvc.perform(post("/domains/{domainId}/groups", USER_DOMAIN_ID)
+    void createAuthority() throws Exception {
+        mockMvc.perform(post("/domains/{domainId}/authorities", USER_DOMAIN_ID)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .header(DOMAIN_AUTH_TOKEN_FIELD, DOMAIN_TOKEN)
-                .content(objectMapper.writeValueAsString(request)))
+                .content(newAuthorityRequest()))
                 .andExpect(status().isCreated())
-                .andDo(document(DOCUMENTATION_NAME, relaxedRequestFields(
-                        fieldWithPath("identifier").description("The role identifier"),
-                        fieldWithPath("name")
-                                .description("The name of the role"),
-                        fieldWithPath("description").description("A brief description of the role"),
-                        fieldWithPath("authorities").description("A list of authority identifiers to be granted to this role")
+                .andDo(document(DOCUMENTATION_NAME, requestFields(
+                        fieldWithPath("[].identifier").description("The authority identifier"),
+                        fieldWithPath("[].name")
+                                .description("The name of the authority"),
+                        fieldWithPath("[].description").description("A brief description of the authority")
                 ), pathParameters(
                         parameterWithName("domainId").description("The domain id of the user")
                 )));
     }
 
-    private void createAndSaveGroup() {
-        group = new Group();
-        group.setDomainId(USER_DOMAIN_ID);
-        group.setIdentifier("sample");
-        group.setDescription("Sample Group");
-        group.setName("Sample Group");
-        group = groupRepository.save(group);
+    private String newAuthorityRequest() {
+        ArrayNode node = objectMapper.createArrayNode();
+        ObjectNode item = node.addObject()
+                .put("identifier", "app.view.authority")
+                .put("name", "View Entities")
+                .put("description", "View all Entities");
+        return node.toPrettyString();
+    }
+
+    private void createAndSaveAuthority() {
+        authority = new Authority();
+        authority.setDomainId(USER_DOMAIN_ID);
+        authority.setIdentifier("sample");
+        authority.setDescription("Sample Authority");
+        authority.setName("Sample Authority");
+        authority = authorityRepository.save(authority);
     }
 
     @Test
-    void addUserToGroup() throws Exception {
-        createAUserAndSave();
-        createAndSaveGroup();
-        mockMvc.perform(RestDocumentationRequestBuilders.post("/domains/{domainId}/groups/{groupIdentifier}/users/{userId}",
-                USER_DOMAIN_ID, group.getIdentifier(), USER_EXTERNAL_ID)
-                .header(DOMAIN_AUTH_TOKEN_FIELD, DOMAIN_TOKEN)
-        )
-                .andExpect(status().isNoContent())
-                .andDo(document(DOCUMENTATION_NAME,
-                        pathParameters(
-                                parameterWithName("domainId").description("The domain id of the group"),
-                                parameterWithName("groupIdentifier").description("The group identifier"),
-                                parameterWithName("userId").description("The id of the user")
-                        )));
-    }
-
-    private ResponseFieldsSnippet usersResponseFields() {
-        return relaxedResponseFields(
-                fieldWithPath("content.[].id").description("The user's unique identification"),
-                fieldWithPath("content.[].username").description("Username of the user"),
-                fieldWithPath("content.[].password").description("password of the user"),
-                fieldWithPath("content.[].roles").type("string list")
-                        .description("List of roles assigned to this user"),
-                fieldWithPath("content.[].authorities").type("string list")
-                        .description("List of authorities given to ths user")
-        );
-    }
-
-    @Test
-    void removeUserFromGroup() throws Exception {
-        createAUserAndSave();
-        createAndSaveGroup();
-        setUserGroup();
-        mockMvc.perform(RestDocumentationRequestBuilders.delete("/domains/{domainId}/groups/{groupIdentifier}/users/{userId}",
-                USER_DOMAIN_ID, group.getIdentifier(), USER_EXTERNAL_ID)
-                .header(DOMAIN_AUTH_TOKEN_FIELD, DOMAIN_TOKEN)
-        )
-                .andExpect(status().isNoContent())
-                .andDo(document(DOCUMENTATION_NAME,
-                        pathParameters(
-                                parameterWithName("domainId").description("The domain id of the group"),
-                                parameterWithName("groupIdentifier").description("The group identifier"),
-                                parameterWithName("userId").description("The id of the user")
-                        )));
-        assertEquals(0, userGroupRepository.findAll().size());
-    }
-
-    private void setUserGroup() {
-        UserGroup userGroup = new UserGroup();
-        userGroup.setUser(appUserRepository.findById(TEST_USER_ID).get());
-        userGroup.setGroup(group);
-        userGroup.setDomainId(USER_DOMAIN_ID);
-        userGroupRepository.save(userGroup);
-    }
-
-    private void createAUserAndSave() {
-        AppUser user = createAppUserWithConstantId();
-        appUserRepository.save(user);
-    }
-
-    @Test
-    void gettingAllUsersInAGroup() throws Exception {
-        List<AppUser> appUserList = createMultipleUsers(3);
-        appUserRepository.saveAll(appUserList);
-        createAndSaveGroup();
-        setToGroup(appUserList);
-        mockMvc.perform(RestDocumentationRequestBuilders.get("/domains/{domainId}/groups/{groupIdentifier}/users",
-                USER_DOMAIN_ID, group.getIdentifier())
+    void getAllAuthoritiesInADomain() throws Exception {
+        createAndSaveAuthority();
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/domains/{domainId}/authorities",
+                USER_DOMAIN_ID)
                 .param("page", "0")
                 .param("size", "10")
                 .header(DOMAIN_AUTH_TOKEN_FIELD, DOMAIN_TOKEN)
@@ -153,22 +90,87 @@ public class AuthorityManagementDocumentation extends AbstractDocumentation {
                                 parameterWithName("size").description("maximum number of items in page")
                         ),
                         pathParameters(
-                                parameterWithName("domainId").description("The domain id of the group"),
-                                parameterWithName("groupIdentifier").description("The group identifier")
+                                parameterWithName("domainId").description("The domain id of the authority")
                         ),
-                        usersResponseFields()));
+                        authorityResponseFields()));
     }
 
-    private void setToGroup(List<AppUser> appUserList) {
-        appUserList
-                .stream()
-                .map(user -> {
-                    UserGroup userGroup = new UserGroup();
-                    userGroup.setGroup(group);
-                    userGroup.setUser(user);
-                    userGroup.setDomainId(USER_DOMAIN_ID);
-                    return userGroup;
-                })
-                .forEach(userGroupRepository::save);
+    private ResponseFieldsSnippet authorityResponseFields() {
+        return relaxedResponseFields(
+                fieldWithPath("content.[].name").description("The name of the authority"),
+                fieldWithPath("content.[].identifier").description("The authority identifier"),
+                fieldWithPath("content.[].description").description("A brief description of the authority")
+        );
+    }
+
+    @Test
+    void deleteAuthority() throws Exception {
+        createAndSaveAuthority();
+        mockMvc.perform(RestDocumentationRequestBuilders.delete("/domains/{domainId}/authorities/{authorityIdentifier}",
+                USER_DOMAIN_ID, authority.getIdentifier())
+                .header(DOMAIN_AUTH_TOKEN_FIELD, DOMAIN_TOKEN)
+        ).andExpect(status().isNoContent());
+        assertTrue(
+                authorityRepository.findById(authority.getId()).isEmpty()
+        );
+    }
+
+    @Test
+    void grantUserAuthority() throws Exception {
+        createAUserAndSave();
+        createAndSaveAuthority();
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/domains/{domainId}/authorities/users/{userId}/addAuthorities",
+                USER_DOMAIN_ID, USER_EXTERNAL_ID)
+                .header(DOMAIN_AUTH_TOKEN_FIELD, DOMAIN_TOKEN)
+                .content(
+                        objectMapper.createArrayNode()
+                                .add(authority.getIdentifier())
+                                .toPrettyString()
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isNoContent())
+                .andDo(document(DOCUMENTATION_NAME,
+                        pathParameters(
+                                parameterWithName("domainId").description("The domain id of the authority"),
+                                parameterWithName("userId").description("The id of the user")
+                        )));
+    }
+
+    @Test
+    void removeUserFromAuthority() throws Exception {
+        createAUserAndSave();
+        createAndSaveAuthority();
+        setUserAuthority();
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/domains/{domainId}/authorities/users/{userId}/removeAuthorities",
+                USER_DOMAIN_ID, USER_EXTERNAL_ID)
+                .header(DOMAIN_AUTH_TOKEN_FIELD, DOMAIN_TOKEN)
+                .content(
+                        objectMapper.createArrayNode()
+                                .add(authority.getIdentifier())
+                                .toPrettyString()
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isNoContent())
+                .andDo(document(DOCUMENTATION_NAME,
+                        pathParameters(
+                                parameterWithName("domainId").description("The domain id of the authority"),
+                                parameterWithName("userId").description("The id of the user")
+                        )));
+        assertEquals(0, userAuthorityRepository.findAll().size());
+    }
+
+    private void setUserAuthority() {
+        UserAuthority userAuthority = new UserAuthority();
+        userAuthority.setUser(appUserRepository.findById(TEST_USER_ID).get());
+        userAuthority.setAuthority(authority);
+        userAuthority.setDomainId(USER_DOMAIN_ID);
+        userAuthorityRepository.save(userAuthority);
+    }
+
+    private void createAUserAndSave() {
+        AppUser user = createAppUserWithConstantId();
+        appUserRepository.save(user);
     }
 }
