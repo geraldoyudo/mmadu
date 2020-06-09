3979071136fbdd43899e882e15d23a027c60a648
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-06-09 09:34:08 +0100

    added role management controller tests

--- mmadu-user-service/src/main/java/com/mmadu/service/models/SaveRoleRequest.java
+++ mmadu-user-service/src/main/java/com/mmadu/service/models/SaveRoleRequest.java
@@ -2,12 +2,20 @@ package com.mmadu.service.models;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class SaveRoleRequest {
+    @NotEmpty(message = "identifier is required")
     private String identifier;
+    @NotEmpty(message = "name is required")
     private String name;
+    @NotEmpty(message = "description is required")
     private String description;
+    @NotNull(message = "authorities is required")
+    @Size(min = 1, message = "authorities is required")
     private List<String> authorities;
 }
