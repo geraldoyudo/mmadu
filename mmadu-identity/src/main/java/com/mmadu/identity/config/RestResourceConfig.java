9501c23b9bc4360fb6c68fcaf040c5318ff794d8
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-06-14 15:08:31 +0100

    implemented oauth2 client with admin credentials

--- mmadu-identity/src/main/java/com/mmadu/identity/config/RestResourceConfig.java
+++ mmadu-identity/src/main/java/com/mmadu/identity/config/RestResourceConfig.java
@@ -5,22 +5,50 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class RestResourceConfig {
 
-    public static final String DOMAIN_AUTH_TOKEN_HEADER = "domain-auth-token";
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .clientCredentials()
+                        .build();
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
 
     @Bean
     @Qualifier("userService")
     public WebClient userServiceClient(@Value("${mmadu.userService.url}") String userServiceUrl,
-                                       @Value("${mmadu.domainKey}") String domainKey) {
+                                       OAuth2AuthorizedClientManager authorizedClientManager
+    ) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth2Client.setDefaultClientRegistrationId("mmadu");
+        oauth2Client.setDefaultOAuth2AuthorizedClient(true);
         return WebClient.builder()
+                .apply(oauth2Client.oauth2Configuration())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader(DOMAIN_AUTH_TOKEN_HEADER, domainKey)
                 .baseUrl(userServiceUrl)
                 .build();
     }
--- mmadu-identity/src/test/java/com/mmadu/identity/providers/users/MmaduUserServiceImplTest.java
+++ mmadu-identity/src/test/java/com/mmadu/identity/providers/users/MmaduUserServiceImplTest.java
@@ -6,6 +6,7 @@ import com.mmadu.identity.services.user.MmaduUserService;
 import com.mmadu.identity.services.user.MmaduUserServiceImpl;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockserver.client.MockServerClient;
@@ -17,8 +18,12 @@ import org.mockserver.model.MediaType;
 import org.mockserver.verify.VerificationTimes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.TestPropertySource;
@@ -41,13 +46,18 @@ import static org.mockserver.model.JsonBody.json;
 @MockServerSettings(ports = 18000)
 @TestPropertySource(properties = {
         "mmadu.userService.url=http://localhost:18000",
-        "mmadu.domainKey=2222"
+        "spring.security.oauth2.client.provider.mmadu.token-uri=http://localhost:18000/clients/token"
 })
+@ImportAutoConfiguration(OAuth2ClientAutoConfiguration.class)
+@EnableAutoConfiguration
 class MmaduUserServiceImplTest {
+    public static final String AUTHORIZATION_HEADER = "Bearer eyJraWQiOiIxMjMiLCJhbGciOiJSUzI1NiJ9.eyJkb21haW5faWQiOiIwIiwic3ViIjoiNWVlNjFkNDE0Y2QyNTc3NWQ0MzExMzYyIiwiYXVkIjoidGVzdCIsIm5iZiI6IjE1OTIxMzkwNzMiLCJpc3MiOiJtbWFkdS5jb20iLCJleHAiOiIxNTkyMjI1NDczIiwiaWF0IjoiMTU5MjEzOTA3MyIsImF1dGhvcml0aWVzIjpbImEuKi4qKiIsInIuKi4qKiJdLCJqdGkiOiI4ODA2YjMzYy1mMDExLTRlNzUtOTI4My04YzJiMDg2MmQ3NzkiLCJjbGllbnRfaWQiOiI1ZjY1ZDBjZC02MTAzLTQ4MzgtOTc2Ni05MTMyNTg5ZTdlODkifQ.J3Bir5mmAmFsmkehDq9a_9R4c23U2cs52V8HGklIUYLR0KF2V_zsSnen8KwMPJYVNKdmJN1LDfDUNZKsPKNEXikrj4Hj4bEmdXfWHYAAXCM-tqiI0VI9TyETy_AVR3QQ_wP4DwVGKc0hioQUiyCOhLTdJtAhSed-yQDG9krTUeFnUMoMhwuMKRLgI2S_3jDose46Az84Xm2JFGMoBBe0b0waZOcidezfoUmqlTkPrU5v9kuNkS6ayLyA0l93u0DAyBQb7xVlY7bsKEiOf3-RwR038eybt8Bi64NInsmBCTLKyv9FDy5Qc_WqiQz1TmIp0v6FE5ZWdYdVLZkQJ66bdQ";
     @Autowired
     private MmaduUserService userService;
     @Value("classpath:responses/get-user-by-username-and-domain.json")
     private Resource userFile;
+    @Value("classpath:responses/client-credentials-token.json")
+    private Resource tokenfile;
     @Value("classpath:requests/user-authentication-request.json")
     private Resource userAuthenticateRequest;
     @Value("classpath:responses/user-authentication-response.json")
@@ -56,6 +66,12 @@ class MmaduUserServiceImplTest {
     private Resource invalidUsernameResponse;
     @Value("classpath:responses/user-authentication-password-invalid.json")
     private Resource invalidPasswordResponse;
+    private HttpRequest tokenRequest;
+
+    @BeforeEach
+    void setUp(MockServerClient server) throws Exception {
+        tokenRequest = mockTokenApi(server);
+    }
 
     @AfterEach
     void reset(MockServerClient server) {
@@ -73,8 +89,34 @@ class MmaduUserServiceImplTest {
                 () -> assertEquals(List.of("admin"), user.getRoles()),
                 () -> assertEquals(List.of("edit"), user.getAuthorities()),
                 () -> assertEquals(List.of("test", "sample"), user.getGroups()),
+                () -> server.verify(tokenRequest, VerificationTimes.exactly(1)),
                 () -> server.verify(request, VerificationTimes.exactly(1))
         );
+    }
+
+    private HttpRequest mockTokenApi(MockServerClient server) throws IOException {
+        HttpRequest request = request()
+                .withMethod("POST")
+                .withPath("/clients/token")
+                .withHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=UTF-8")
+                .withHeader(HttpHeaders.AUTHORIZATION, "Basic NWY2NWQwY2QtNjEwMy00ODM4LTk3NjYtOTEzMjU4OWU3ZTg5OjExMzIzMjMyMw==")
+                .withHeader(HttpHeaders.USER_AGENT, "Java/14.0.1")
+                .withHeader(HttpHeaders.ACCEPT, "application/json;charset=UTF-8")
+                .withHeader(HttpHeaders.HOST, "localhost:18000")
+                .withHeader(HttpHeaders.CONNECTION, "keep-alive")
+                .withHeader(HttpHeaders.CONTENT_LENGTH, "49")
+                .withBody("grant_type=client_credentials&scope=a.*.**+r.*.**");
+        server
+                .when(request)
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                                .withBody(
+                                        IOUtils.toString(tokenfile.getInputStream(), StandardCharsets.UTF_8),
+                                        MediaType.JSON_UTF_8
+                                )
+                );
+        return request;
     }
 
     private HttpRequest mockUserApi(MockServerClient server) throws IOException {
@@ -82,7 +124,7 @@ class MmaduUserServiceImplTest {
                 .withMethod("GET")
                 .withPath("/domains/1/users/load")
                 .withQueryStringParameter("username", "test")
-                .withHeader("domain-auth-token", "2222");
+                .withHeader(HttpHeaders.AUTHORIZATION, AUTHORIZATION_HEADER);
         server.when(
                 request
         )
@@ -103,6 +145,7 @@ class MmaduUserServiceImplTest {
         Optional<MmaduUser> user = userService.loadUserByUsernameAndDomainId("test", "1");
         assertAll(
                 () -> assertTrue(user.isEmpty()),
+                () -> server.verify(tokenRequest, VerificationTimes.exactly(1)),
                 () -> server.verify(request, VerificationTimes.exactly(1))
         );
     }
@@ -112,7 +155,7 @@ class MmaduUserServiceImplTest {
                 .withMethod("GET")
                 .withPath("/domains/1/users/load")
                 .withQueryStringParameter("username", "test")
-                .withHeader("domain-auth-token", "2222");
+                .withHeader(HttpHeaders.AUTHORIZATION, AUTHORIZATION_HEADER);
         server.when(
                 request
         )
@@ -140,7 +183,7 @@ class MmaduUserServiceImplTest {
                                 MatchType.STRICT
                         )
                 )
-                .withHeader("domain-auth-token", "2222");
+                .withHeader(HttpHeaders.AUTHORIZATION, AUTHORIZATION_HEADER);
         server.when(
                 request
         )
@@ -160,6 +203,7 @@ class MmaduUserServiceImplTest {
         HttpRequest request = mockAuthenticateApi(server, invalidPasswordResponse);
         assertThrows(BadCredentialsException.class,
                 () -> userService.authenticate("1", "user", "password"));
+        server.verify(tokenRequest, VerificationTimes.exactly(1));
         server.verify(request, VerificationTimes.once());
     }
 
@@ -168,6 +212,7 @@ class MmaduUserServiceImplTest {
         HttpRequest request = mockAuthenticateApi(server, invalidUsernameResponse);
         assertThrows(UsernameNotFoundException.class,
                 () -> userService.authenticate("1", "user", "password"));
+        server.verify(tokenRequest, VerificationTimes.exactly(1));
         server.verify(request, VerificationTimes.once());
     }
 }
