4d5c78df9af9375bfe31654e9a1d13c961137ef0
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-06-14 12:55:53 +0100

    updated security configuration

--- mmadu-registration/src/main/java/com/mmadu/registration/config/WebSecurityConfig.java
+++ mmadu-registration/src/main/java/com/mmadu/registration/config/WebSecurityConfig.java
@@ -1,14 +1,15 @@
 package com.mmadu.registration.config;
 
+import com.mmadu.security.api.MmaduWebSecurityConfigurer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends MmaduWebSecurityConfigurer {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
         http
                 .csrf().disable()
                 .authorizeRequests()
