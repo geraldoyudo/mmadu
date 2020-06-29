8f1c4c00622f60b76891940dde0a3d014b380902
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-06-29 18:20:57 +0100

    bumped version to 2.0.0

--- docker/prod/release.sh
+++ docker/prod/release.sh
@@ -1,3 +1,3 @@
-docker push geraldoyudo/mmadu-identity-service:2.0.0-SNAPSHOT
-docker push geraldoyudo/mmadu-registration:2.0.0-SNAPSHOT
-docker push geraldoyudo/mmadu-user-service:2.0.0-SNAPSHOT
+docker push geraldoyudo/mmadu-identity-service:2.0.0
+docker push geraldoyudo/mmadu-registration:2.0.0
+docker push geraldoyudo/mmadu-user-service:2.0.0
