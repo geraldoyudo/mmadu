e44b04b1c94385e21ae5e4587dbb2c086264fca2
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-07-03 10:35:22 +0100

    used a jwt user details service

--- samples/java/authorization-springboot/src/main/java/com/gerald/samples/mmadu/springboot/DefaultOauth2UserService.java
+++ samples/java/authorization-springboot/src/main/java/com/gerald/samples/mmadu/springboot/DefaultOauth2UserService.java
@@ -1,5 +1,9 @@
 package com.gerald.samples.mmadu.springboot;
 
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -8,16 +12,29 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class DefaultOauth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
-        return new DefaultOAuth2User(List.of(new SimpleGrantedAuthority("admin")),
-                Map.of("username", "gerald"), "username");
+        try {
+            JWT jwt = JWTParser.parse(oAuth2UserRequest.getAccessToken().getTokenValue());
+            JWTClaimsSet claims = jwt.getJWTClaimsSet();
+            String scope = Optional.ofNullable(claims.getStringClaim("scope")).orElse("");
+            return new DefaultOAuth2User(toAuthorities(scope), claims.getClaims(), "username");
+        } catch (Exception ex) {
+            throw new IllegalStateException("Could not parse user", ex);
+        }
+    }
+
+    private List<GrantedAuthority> toAuthorities(String scope) {
+        return Arrays.stream(scope.split("[ ]"))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 }
--- samples/java/authorization-springboot/src/main/java/com/gerald/samples/mmadu/springboot/SecurityConfig.java
+++ samples/java/authorization-springboot/src/main/java/com/gerald/samples/mmadu/springboot/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests().anyRequest().hasAuthority("super_admin")
                 .and()
                 .oauth2Login()
                 .userInfoEndpoint()
