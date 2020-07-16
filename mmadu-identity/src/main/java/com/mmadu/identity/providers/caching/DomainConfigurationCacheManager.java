882fcfbec895e441000c029abda75fadcd1bd817
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-07-16 12:27:45 +0100

    implemented cache evict for existing caches identity service

--- mmadu-identity/src/main/java/com/mmadu/identity/providers/caching/DomainConfigurationCacheManager.java
+++ mmadu-identity/src/main/java/com/mmadu/identity/providers/caching/DomainConfigurationCacheManager.java
@@ -1,6 +1,7 @@
 package com.mmadu.identity.providers.caching;
 
 import com.mmadu.identity.entities.DomainIdentityConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.rest.core.annotation.HandleAfterDelete;
@@ -8,15 +9,19 @@ import org.springframework.data.rest.core.annotation.HandleAfterSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RepositoryEventHandler
-@CacheConfig(cacheNames = "domainConfigurations")
+@CacheConfig(cacheNames = {
+        "domainConfigurations",
+        "verificationKeys"
+})
 public class DomainConfigurationCacheManager {
 
     @HandleAfterSave
     @HandleAfterDelete
     @CacheEvict(key = "#configuration.domainId")
     public void updateDomainConfigurationCache(DomainIdentityConfiguration configuration) {
-
+        log.debug("Evicting domain configuration cache for domain {}", configuration.getDomainId());
     }
 }
