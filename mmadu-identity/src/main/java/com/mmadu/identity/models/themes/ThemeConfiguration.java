50deeb8ac1572b58025fb8a004a31315ea614cfb
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-08-06 17:23:03 +0100

    added theme configuration to authorization page

--- mmadu-identity/src/main/java/com/mmadu/identity/models/themes/ThemeConfiguration.java
+++ mmadu-identity/src/main/java/com/mmadu/identity/models/themes/ThemeConfiguration.java
@@ -6,6 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties(prefix = "mmadu.identity.theme")
 public class ThemeConfiguration {
-    private String iconSvg;
+    private String logoSvg;
     private ThemeColour themeColour = new ThemeColour();
 }
