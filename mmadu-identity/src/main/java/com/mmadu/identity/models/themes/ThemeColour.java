9766a6151162e014b18583b2bf0b756791df7286
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-08-06 17:47:00 +0100

    tested identity theming

--- mmadu-identity/src/main/java/com/mmadu/identity/models/themes/ThemeColour.java
+++ mmadu-identity/src/main/java/com/mmadu/identity/models/themes/ThemeColour.java
@@ -4,15 +4,15 @@ import lombok.Data;
 
 @Data
 public class ThemeColour {
-    private String primary = "lightcoral";
-    private String secondary = "darkslateblue";
-    private String info = "aliceblue";
-    private String primaryDark = "#d07070";
-    private String primaryDarkest = "#c06060";
-    private String secondaryDark = "#39306f";
-    private String secondaryDarkest = "#2b2453";
-    private String danger = "darkred";
-    private String dangerText = "white";
-    private String success = "#2b8378";
-    private String successText = "black";
+    private String primary = "#007D76";
+    private String secondary = "#E6354C";
+    private String info = "#53FCE6";
+    private String primaryDark = "#005752";
+    private String primaryDarkest = "rgba(38, 145, 139, 0.5)";
+    private String secondaryDark = "#ce1a31";
+    private String secondaryDarkest = "rgba(234, 83, 103, 0.5)";
+    private String danger = "#d5d5d5";
+    private String dangerText = "#161616";
+    private String success = "##feffff";
+    private String successText = "#838584";
 }
