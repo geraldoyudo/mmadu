c144d2ed816ebf80c31f0d13cdc4b579ab226f8d
Oyudo Gerald <gerald.oyudo@gmail.com>
2020-08-12 04:16:43 +0100

    updated docker

--- docker/prod/release.sh
+++ docker/prod/release.sh
@@ -2,3 +2,4 @@ docker push geraldoyudo/mmadu-identity:2.0.3-SNAPSHOT
 docker push geraldoyudo/mmadu-registration:2.0.3-SNAPSHOT
 docker push geraldoyudo/mmadu-user-service:2.0.3-SNAPSHOT
 docker push geraldoyudo/mmadu-notifications:2.0.3-SNAPSHOT
+docker push geraldoyudo/mmadu-otp:2.0.3-SNAPSHOT
